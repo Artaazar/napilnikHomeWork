d16f1def98b242e8cf87a6016d34de4234a97547
Ilya Yaroslavtsev <yarosis@gmail.com>
2021-07-04 23:12:38 +0300

    Первая задача напильника

---
+++ IincapsulationHW/First.cs
@@ -0,0 +1,69 @@
+﻿namespace IincapsulationHW
+{
+    public class Weapon
+    {
+        public int Damage { get; }
+
+        private int _bullets;
+        public int Bullets => _bullets;
+
+        public Weapon(int damage, int bullets)
+        {
+            Damage = damage;
+            _bullets = bullets;
+        }
+
+        public void Fire(Player player)
+        {
+            if (Bullets > 0)
+            {
+                player.TakeDamage(Damage);
+                _bullets -= 1;
+            }
+        }
+    }
+    
+
+    public class Player
+    {
+        public int Health { get; private set; }
+
+        public Player(int hitPoints)
+        {
+            Health = hitPoints;
+        }
+
+        public Player() : this(100)
+        {
+        }
+
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+            if (Health < 0)
+                Health = 0;
+        }
+        public void Heal(int healedPoints)
+        {
+            if (Health <= 0)
+                return;
+
+            Health += healedPoints;
+        }
+    }
+
+    public class Bot
+    {
+        public Weapon Weapon { get; }
+
+        public Bot(Weapon weapon)
+        {
+            Weapon = weapon;
+        }
+
+        public void OnSeePlayer(Player player)
+        {
+            Weapon.Fire(player);
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using IincapsulationHW;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DefaultTest()
+        {
+            var player = new Player();
+            var weapon = new Weapon(2, 2000);
+            var bot = new Bot(weapon);
+            bot.OnSeePlayer(player);
+        }
+    }
+}
