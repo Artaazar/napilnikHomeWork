f4dc43696e2a2e302dbf15db218d6bdb127ec3c6
Ilya Yaroslavtsev <yarosis@gmail.com>
2021-07-08 01:01:12 +0300

    Справлена первая домашка

--- IincapsulationHW/First.cs
+++ IincapsulationHW/First.cs
@@ -1,36 +1,42 @@
-﻿namespace IincapsulationHW
+﻿using System;
+
+namespace IincapsulationHW
 {
     public class Weapon
     {
+        //Разница была по сути в сахаре. То есть никакой - зарабтался, забыл допилить решарпером xD
         public int Damage { get; }
-
-        private int _bullets;
-        public int Bullets => _bullets;
+        public int Bullets { get; private set; }
 
         public Weapon(int damage, int bullets)
         {
             Damage = damage;
-            _bullets = bullets;
+            Bullets = bullets;
         }
-
+        // Если не хватает пуль - это ошибка.Из оружия без патронов стрелять нельзя. 
+        //Можно. Дети стреляют даже из палок, вопрос в том что произойдёт дальше, как это действие должно обрабатываться.
+        //Я читал когда-то про два варианта взаимодействия в рамках проекта: через получаемые результаты (в первоначальнм случае на спусковой крючок нажали, но ружжо щёлкнуло впустую)
+        //Или через бросание эксепшенов и последующую их лювлю. Читал краем глаза и данный вопрос в лекции не поднимался.
         public void Fire(Player player)
         {
-            if (Bullets > 0)
-            {
-                player.TakeDamage(Damage);
-                _bullets -= 1;
-            }
+            if (Bullets <= 0)
+                throw new Exception("Боезапас пуст!");
+            
+            player.TakeDamage(Damage);
+            Bullets -= 1;
         }
     }
-    
+
 
     public class Player
     {
         public int Health { get; private set; }
 
-        public Player(int hitPoints)
+        //У игрока расходится терминология, свойство Health а параметр конструктора почему-то hitPoints
+        //Стогого говоря - да, но так - нет, это синнимы в игровом мире :), но для именования оно да, неверно было выбрано.
+        public Player(int health)
         {
-            Health = hitPoints;
+            Health = health;
         }
 
         public Player() : this(100)
@@ -43,27 +49,26 @@
             if (Health < 0)
                 Health = 0;
         }
-        public void Heal(int healedPoints)
-        {
-            if (Health <= 0)
-                return;
 
-            Health += healedPoints;
-        }
+        //Зачем метод Heal? 
+        //НУ как... Чтобы лечить. Метод нанесения урона игроку есть, значит должен быть и метод его лечения при подборе аптечки.
+        //Автоматический оверинжиниринг, простите :)
     }
 
     public class Bot
     {
-        public Weapon Weapon { get; }
+        //Зачем у бота Weapon свойством?
+        //Да, косяк. Оно ещё и публичным зачем-то было.
+        private readonly Weapon _weapon;
 
         public Bot(Weapon weapon)
         {
-            Weapon = weapon;
+            _weapon = weapon;
         }
 
         public void OnSeePlayer(Player player)
         {
-            Weapon.Fire(player);
+            _weapon.Fire(player);
         }
     }
 }
